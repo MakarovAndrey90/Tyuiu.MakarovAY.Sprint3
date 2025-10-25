2af51d9266fa40508aa204b9996100efe14d6eda
Андрей Макаров <tyuiu.MakarovAY@outlook.com>
2025-10-25 20:18:13 +0500

    Task0

---
+++ Tyuiu.MakarovAY.Sprint3.Task0.V5.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.MakarovAY.Sprint3.Task0.V5.Lib;
+namespace Tyuiu.MakarovAY.Sprint3.Task0.V5.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetSumSeries()
+        {
+            DataService ds = new DataService();
+            int startValue = 1;
+            int stopValue = 10;
+            double res = ds.GetSumSeries(startValue, stopValue);
+            double wait = 81.083;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.MakarovAY.Sprint3.Task0.V5/Program.cs
@@ -0,0 +1,38 @@
+﻿using Tyuiu.MakarovAY.Sprint3.Task0.V5.Lib;
+namespace Tyuiu.MakarovAY.Sprint3.Task0.V5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #2 | Выполнил: Макаров А. Я. | РППБ-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Тема: операторо цикла for                                               *");
+            Console.WriteLine("* Задание #0                                                              *");
+            Console.WriteLine("* Вариант #5                                                              *");
+            Console.WriteLine("* Выполнил: Макаров А. Я. | РППБ-25-1                                     *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу используя цикл for, которая вычисляет сумму ряда     *");
+            Console.WriteLine("* по формуле                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int startValue = 1; int stopValue = 10;
+
+            Console.WriteLine("Старт шага = " + startValue);
+            Console.WriteLine("Конец шага = " + stopValue);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Сумма ряда = " + ds.GetSumSeries(startValue, stopValue));
+            Console.ReadKey();
+
+        }
+    }
+}
