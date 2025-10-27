42d6e4a6a3873a1ba79c3e54efcd32e9a204dc84
Андрей Макаров <tyuiu.MakarovAY@outlook.com>
2025-10-27 20:13:29 +0500

    Task1

--- Tyuiu.MakarovAY.Sprint3.Task1.V13/Program.cs
+++ Tyuiu.MakarovAY.Sprint3.Task1.V13/Program.cs
@@ -6,10 +6,10 @@ namespace Tyuiu.MakarovAY.Sprint3.Task1.V13
         static void Main(string[] args)
         {
             DataService ds = new DataService();
-            Console.Title = "Спринт #2 | Выполнил: Макаров А. Я. | РППБ-25-1";
+            Console.Title = "Спринт #3 | Выполнил: Макаров А. Я. | РППБ-25-1";
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* Спринт #3                                                               *");
-            Console.WriteLine("* Тема: операторо цикла while                                             *");
+            Console.WriteLine("* Тема: операторор цикла while                                            *");
             Console.WriteLine("* Задание #1                                                              *");
             Console.WriteLine("* Вариант #13                                                             *");
             Console.WriteLine("* Выполнил: Макаров А. Я. | РППБ-25-1                                     *");
