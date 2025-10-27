08b9a8181ea75bebbabdafbedd756206f2045566
Андрей Макаров <tyuiu.MakarovAY@outlook.com>
2025-10-27 20:13:04 +0500

    Task2

---
+++ Tyuiu.MakarovAY.Sprint3.Task2.V26.Lib/DataService.cs
@@ -0,0 +1,17 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+namespace Tyuiu.MakarovAY.Sprint3.Task2.V26.Lib
+{
+    public class DataService : ISprint3Task2V26
+    {
+        public double GetMultiplySeries(double value, int startValue, int stopValue)
+        {
+            double SumSeries = 1;
+            do 
+            {
+                SumSeries = SumSeries * ((Math.Pow(value, 3) * startValue) + 2);
+                startValue++;
+            } while (startValue <= stopValue);
+            return Math.Round(SumSeries, 3);
+        }
+    }
+}
---
+++ Tyuiu.MakarovAY.Sprint3.Task2.V26/Program.cs
@@ -0,0 +1,40 @@
+﻿using Tyuiu.MakarovAY.Sprint3.Task2.V26.Lib;
+namespace Tyuiu.MakarovAY.Sprint3.Task2.V26
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #3 | Выполнил: Макаров А. Я. | РППБ-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Тема: операторор цикла do-while                                         *");
+            Console.WriteLine("* Задание #2                                                              *");
+            Console.WriteLine("* Вариант #26                                                             *");
+            Console.WriteLine("* Выполнил: Макаров А. Я. | РППБ-25-1                                     *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу используя цикл do...while, которая вычисляет         *");
+            Console.WriteLine("* произведение ряда по формуле, при х=0,25                                *");             
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int startValue = 1; int stopValue = 17;
+            double value = 0.25;
+
+            Console.WriteLine("переменная X = " + value);
+            Console.WriteLine("Старт шага = " + startValue);
+            Console.WriteLine("Конец шага = " + stopValue);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Произведение ряда = " + ds.GetMultiplySeries(value, startValue, stopValue));
+            Console.ReadKey();
+
+        }
+    }
+}
