12183749ae59e4797cfb41bc65ef1ca3c1787a74
Андрей Макаров <tyuiu.MakarovAY@outlook.com>
2025-10-31 14:09:04 +0500

    Task7

---
+++ Tyuiu.MakarovAY.Sprint3.Task7.V18.Test/DataServiceTest.cs
@@ -0,0 +1,34 @@
+﻿using Tyuiu.MakarovAY.Sprint3.Task7.V18.Lib; 
+namespace Tyuiu.MakarovAY.Sprint3.Task7.V18.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetMassFunction()
+        {
+            DataService ds = new DataService(); 
+            int startValue = -5; int stopValue = 5;
+            int len = stopValue - startValue + 1;
+            double[] ValueWaitArray;
+            ValueWaitArray = new double[len];
+            ValueWaitArray[0] = 7.76;
+            ValueWaitArray[1] = 7.36;
+            ValueWaitArray[2] = 7.26;
+            ValueWaitArray[3] = 7.9;
+            ValueWaitArray[4] = 15.88;
+            ValueWaitArray[5] = 2;
+            ValueWaitArray[6] = 4.35;
+            ValueWaitArray[7] = 5.63;
+            ValueWaitArray[8] = 6.49;
+            ValueWaitArray[9] = 6.49;
+            ValueWaitArray[10] = 6.32;
+            double[] res;
+            res = new double[len];
+            res = ds.GetMassFunction(startValue, stopValue);
+            CollectionAssert.AreEqual(ValueWaitArray, res);
+
+
+        }
+    }
+}
---
+++ Tyuiu.MakarovAY.Sprint3.Task7.V18/Program.cs
@@ -0,0 +1,56 @@
+﻿using System.Runtime.Serialization.Formatters;
+using Tyuiu.MakarovAY.Sprint3.Task7.V18.Lib;
+namespace Tyuiu.MakarovAY.Sprint3.Task7.V18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #3 | Выполнил: Макаров А. Я. | РППБ-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Тема: Добавление к решению итоговых проектов по спринту                 *");
+            Console.WriteLine("* Задание #7                                                              *");
+            Console.WriteLine("* Вариант #18                                                             *");
+            Console.WriteLine("* Выполнил: Макаров А. Я. | РППБ-25-1                                     *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая выводит таблицу значений функции F(x)       *");
+            Console.WriteLine("* (произвести табулирование) f(x) на заданном диапазоне [-5; 5]           *");
+            Console.WriteLine("*            F(x) =    2x - 3                                             *");
+            Console.WriteLine("*                    ----------  + 5                                      *");
+            Console.WriteLine("*                    cos(x) + x                                           *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int startValue = -5; int stopValue = 5;
+            int len = ds.GetMassFunction(startValue, stopValue).Length;
+            double[] ValueArray;
+            ValueArray = new double[len];
+            ValueArray = ds.GetMassFunction(startValue, stopValue);
+
+
+
+            Console.WriteLine("* Старт Шага = " + startValue);
+            Console.WriteLine("* Конец Шага = " + stopValue);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("+----------+----------+");
+            Console.WriteLine("|    X     |   F(x)   |");
+            Console.WriteLine("+----------+----------+");
+            for (int i = 0; i <= len - 1; i++) 
+            {
+                Console.WriteLine("| {0,4:d}     |  {1, 5:f2}   |", startValue, ValueArray[i]);
+                startValue++;
+            }
+            Console.WriteLine("+----------+----------+");
+            Console.ReadKey();
+
+        }
+    }
+}
